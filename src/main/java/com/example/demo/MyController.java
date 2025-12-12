e38e6c4c91af644607e4426dbde593f58674b72a
Aditya_Phuge <adiphuge712@gmail.com>
2025-12-12 14:32:44 +0530

    my second commit

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -3,31 +3,30 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class MyController {
 	@GetMapping("/")
 	public String home() {
 		return "home";// home.jsp
-		//internalresourseviewer is set to the application.propertiesfile
+		// internalresourseviewer is set to the application.propertiesfile
 	}
-	
+
 	@GetMapping("/loging")
 	public String loging() {
 		return "login";// home.jsp
-		//internalresourseviewer is set to the application.propertiesfile
+		// internalresourseviewer is set to the application.propertiesfile
 	}
-	
+
 	@GetMapping("/about")
 	public String about() {
 		return "About";// home.jsp
-		//internalresourseviewer is set to the application.propertiesfile
+		// internalresourseviewer is set to the application.propertiesfile
 	}
-	
+
 	@GetMapping("/dashboard")
 	public String dashboard() {
 		return "dashboard";// home.jsp
-		//internalresourseviewer is set to the application.propertiesfile
+		// internalresourseviewer is set to the application.propertiesfile
 	}
 
 }
